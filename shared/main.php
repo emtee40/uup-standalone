bd9de475b0ff6717b5875e58d980f919c0e0fe82
orin <orin@git.uupdump.net>
2023-11-03 21:14:24 +0100

    Allow build to be specified as a string

--- shared/main.php
+++ shared/main.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__).'/../api/shared/main.php';
 
 function brand($script = null) {
-    $projVersion = '1.1.4';
+    $projVersion = '1.2.0';
     return 'UUP dump standalone v'.$projVersion;
 }
 
